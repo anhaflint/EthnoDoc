1f6e59280f4237bbc2b3f8d58c95c39e3abad932
anhaflint <anha.flint@gmail.com>
2015-02-17 11:17:57 +0100

    add JMS/Serializer and FOS/elastica-bundle dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new EthnoDoc\PublicationBundle\EthnoDocPublicationBundle(),
+            new \JMS\SerializerBundle\JMSSerializerBundle(),
+            new \FOS\ElasticaBundle\FOSElasticaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
