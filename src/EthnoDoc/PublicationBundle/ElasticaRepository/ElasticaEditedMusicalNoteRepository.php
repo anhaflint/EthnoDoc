5c23694ad3e18be8ae8afa1e2b406e023d38fbcc
anhaflint <anha.flint@gmail.com>
2015-02-19 15:24:44 +0100

    add Elastica Repositories

---
+++ src/EthnoDoc/PublicationBundle/ElasticaRepository/ElasticaEditedMusicalNoteRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace EthnoDoc\PublicationBundle\ElasticaRepository;
+
+use FOS\ElasticaBundle\Repository;
+
+/**
+ *  ElasticaEditedMusicalNoteRepository
+ *
+ */
+class ElasticaEditedMusicalNoteRepository extends Repository
+{
+}
