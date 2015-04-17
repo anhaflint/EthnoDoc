660dda0dfa7e9d479b96337dfd904b79192bc5d0
Anhaflint <anha.flint@gmail.com>
2015-04-17 16:35:33 +0200

    delete unused methods

--- src/EthnoDoc/PublicationBundle/ElasticaRepository/ElasticaEditedMusicalNoteRepository.php
+++ src/EthnoDoc/PublicationBundle/ElasticaRepository/ElasticaEditedMusicalNoteRepository.php
@@ -10,22 +10,5 @@ use FOS\ElasticaBundle\Repository;
  */
 class ElasticaEditedMusicalNoteRepository extends Repository
 {
-    public function findTest($string)
-    {
-        $query_part = new \Elastica\Query\Bool();
-        $query_part->addShould(
-            new \Elastica\Query\Term(
-                array('title' => array('value' => 'title1', 'boost' => 3))
-            )
-        );
 
-        $query_part->addShould(
-            new \Elastica\Query\Term(
-                array('country' => array('value' => 'country6'))
-            )
-        );
-        $query = new \Elastica\Query\Filtered($query_part);
-
-        return $this->findHybrid($query);
-    }
 }
